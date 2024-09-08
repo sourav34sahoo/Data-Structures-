14c221c9ae602e83c499f56d20dc299e3f4161dd
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-08 13:07:20 +0530

    Program for adding default value in an arrray

---
+++ Array/ArrayUtil.java
@@ -0,0 +1,22 @@
+public class ArrayUtil
+{
+	public void printArray(int[] arr)
+	{
+		int n=arr.length;
+		for(int i=0 ; i<arr.length ; i++)
+		{
+			System.out.println(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public void arrayDemo()
+	{
+		int[] myArray = new int[5];
+		printArray(myArray);
+	}
+	public static void main(String...args)
+	{
+		ArrayUtil arrUtil = new ArrayUtil();
+		arrUtil.arrayDemo();
+	}
+}
