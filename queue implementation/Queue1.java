51533a4847039f22f378a4fc807a02476c784d35
sourav sahoo <sahoo34sourav@gmail.com>
2025-05-09 19:13:27 +0530

    Queue implementation

--- queue implementation/Queue1.java
+++ queue implementation/Queue1.java
@@ -5,14 +5,42 @@ public class Queue1{
 	int front;
 	
 	public void enQueue(int data){
-		queue[rear] = data;
-		rear++;
-		size++;
+		if(!isFull()){
+			queue[rear] = data;
+			rear++;
+			size++;
+		}
+		else {
+			System.out.println("Queue is full");
+		}
 	}
+	
+	public int deQueue(){
+		int data = queue[front];
+		if(!isEmpty()){
+			front++;
+			size--;
+		} else {
+			System.out.println("Queue is empty");
+		}
+		return data;
+	}
+	
+	public int getSize()
+	{
+		return size;
+	}
+	public boolean isEmpty(){
+		return size==0;
+	}
+	public boolean isFull(){
+		return size==5;
+	}
+	
 	public void show(){
 		System.out.print("Elements : ");
 		for(int i=0;i<size; i++){
-			System.out.print(queue[i]+" ");
+			System.out.print(queue[front+i]+" ");
 		}
 	}
 }
--- queue implementation/QueueImp.java
+++ queue implementation/QueueImp.java
@@ -4,7 +4,16 @@ public class QueueImp
 		Queue1 num = new Queue1();
 		num.enQueue(18);
 		num.enQueue(29);
+		num.enQueue(45);
+		num.enQueue(79);
+		num.enQueue(45);
+		num.enQueue(79);
 		
+		System.out.println(num.deQueue());
+		System.out.println(num.deQueue());
+		System.out.println("Size : "+num.getSize());
+		System.out.println("Empty : "+num.isEmpty());
+		System.out.println("Full : "+num.isFull());
 		num.show();
 	}
 }
