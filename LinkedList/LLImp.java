a5e3b38c72b6baef99cecedf025d76479fe2007b
sourav sahoo <sahoo34sourav@gmail.com>
2025-05-11 18:22:01 +0530

    LinkedList Implementation

---
+++ LinkedList/LLImp.java
@@ -0,0 +1,13 @@
+public class LLImp
+{
+	public static void main(String...args){
+		
+		LinkedList l = new LinkedList();
+		l.insert(5);
+		l.insert(25);
+		l.insert(35);
+		l.insert(45);
+		
+		l.show();
+	}
+}
